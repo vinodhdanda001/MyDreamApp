9e3dcfa1669b861fff7397f0aeef003847116939
vinodhdanda001 <vinodhdanda001@gmail.com>
2017-04-23 12:05:47 +0530

    Addd table data

---
+++ TrackMyKid/src/Script/000_TableDropScript.sql
@@ -0,0 +1,40 @@
+drop table [dbo].[RoleDetails]
+go
+
+drop table [dbo].[MemberRole]
+go
+
+drop table [dbo].[OrganizationMembers]
+go
+
+drop table  [dbo].[Login]
+go
+
+drop table [dbo].[TripVehicleDriverDetails]
+go
+
+drop table [dbo].[DriverDetails]
+go
+
+drop table [dbo].[VehicleDetails]
+go
+
+drop table [dbo].[TripHaltTimings]
+go
+
+drop table [dbo].[RouteHalts]
+Go
+
+drop table [dbo].[RouteMembers]
+go
+
+drop table [dbo].[RouteTrips]
+go
+
+drop table [dbo].[OrganizationRoutes]
+go
+
+drop table [dbo].[Organization]
+go
+
+
