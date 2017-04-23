c1cc854c37b4977291aa3a8df742e4afca8f2ab5
vinodhdanda001 <vinodhdanda001@gmail.com>
2017-04-24 01:57:45 +0530

    Added log4 net configuration and integrated with test aplicaton for now.

---
+++ TrackMyKid/src/TrackMyKid.Common/LogHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/// <summary>
+/// /This class works only above 4.6 framwork.
+/// </summary>
+namespace TrackMyKid.Common
+{
+    public class LogHelper
+    {
+        public static log4net.ILog GetLogger([CallerFilePath] string fileName = "")
+        {
+            return log4net.LogManager.GetLogger(fileName);
+        }
+    }
+}
