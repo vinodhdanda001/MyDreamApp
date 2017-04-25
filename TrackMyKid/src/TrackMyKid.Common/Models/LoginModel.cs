4f820c8949a2dc03b775f154c2290639625e34e3
vinodhdanda001 <vinodhdanda001@gmail.com>
2017-04-25 10:45:16 +0530

    Added Service file

---
+++ TrackMyKid/src/TrackMyKid.Common/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackMyKid.Common.Models
+{
+    public class LoginModel
+    {
+        public string userName { get; set; }
+        public string passWord { get; set; }
+        public int organizationId { get; set; }
+    }
+}
---
+++ TrackMyKid/src/TrackMyKid.Common/Models/UserProfile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackMyKid.Common.Models
+{
+    public class UserProfile
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public int OrganizationId { get; set; }
+        public string RouteID { get; set; }
+        public string RouteDisplayName { get; set; }
+        public string TripId { get; set; }
+        public string Address { get; set; }
+
+        public string FullName {
+                 get{
+                          return FirstName + " " + LastName;    // MiddleName + " " +
+            }
+        }
+
+        
+  
+                
+                               
+    }
+}
