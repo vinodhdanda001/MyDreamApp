4bb834187802d6738e7b5b89d219825f55a0a3d9
BBB <er.bbbeura@gmail.com>
2017-09-02 09:22:04 +0530

    Tracking map related changes --bbb

---
+++ TrackMyKid/src/TrackMyKid.Web.Management/Controllers/TrackingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrackMyKid.Common.Models;
+using TrackMyKid.Common.ViewModel;
+using TrackMyKid.Web.Management.ApiHelper;
+using TrackMyKid.Web.Management.ApiHelper.Interfaces;
+using static TrackMyKid.Common.Enums.Enumerations;
+
+namespace TrackMyKid.Web.Management.Controllers
+{
+    public class TrackingController : Controller
+    {
+        static readonly IRouteRestClient RouteRestClient = new RouteRestClient();
+        static readonly INotificationRestClient NotificationRestClient = new NotificationRestClient();
+        static readonly ITripRestClient TripRestClient = new TripRestClient();
+
+
+        // GET: Tracking
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
